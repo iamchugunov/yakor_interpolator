5d5ed26be490d1df44001c6fc5b59b18caef7469
Natasha Masalkova <64376822+nawas5@users.noreply.github.com>
2021-04-26 17:03:25 +0300

    Добавили сборку частей посылки

--- main.py
+++ main.py
@@ -17,29 +17,28 @@ while True:
     rcv_size = int.from_bytes(client.recv(4), "little")
     rcv_type = int.from_bytes(client.recv(4), "little")
 
-
-
     print("Size {}".format(rcv_size))
     print("Type {:0x}".format(rcv_type))
 
-    data = client.recv(rcv_size)
-    print(len(data))
+    data = client.recv(rcv_size) #20000
+
+    last_bytes = rcv_size - len(data)
+
+    while last_bytes > 0:
+        data = data + client.recv(last_bytes)
+        last_bytes = rcv_size - len(data)
+
     if rcv_type == 0x150001 or rcv_type == 0x150002:
-        print(data)
-        print(len(data))
-        data = data.decode()
-        print(data)
-        data = json.loads(data)
-        print(data)
-        # data = json.loads((data.decode()))
+        data = json.loads((data.decode()))
 
         if rcv_type == 0x150001:
             pr.process_initial_data(data, config)
 
         if rcv_type == 0x150002:
             if config.ini_data_flag:
-                print("sosi")
-                print(data)
+                points = data["meas"]
+                print(len(points))
+                print(points)
                 # pr.process_measurements(data, config)
                 # N = len(data['trajPoints'])
                 # track = {}
